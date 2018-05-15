d912178aa89eab7f14369367ab892af99765be8a
tonyfast <tony.fast@gmail.com>
2018-05-15 11:56:20 -0400

    I like dirt

--- nostalgiaforever/__init__.py
+++ nostalgiaforever/__init__.py
@@ -1,2 +1,7 @@
+from pidgin.markdown import load_ipython_extension
+load_ipython_extension()
+from pidgin.conventions import load_ipython_extension
+load_ipython_extension()
+
 with __import__('importnb').Notebook():
     from . import importing, testing, reuse
