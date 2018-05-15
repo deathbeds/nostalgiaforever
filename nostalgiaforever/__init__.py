0731d481a4acb6f44e78468e911b86fc6a23166f
tonyfast <tony.fast@gmail.com>
2018-05-15 12:05:37 -0400

    include notebooks

--- nostalgiaforever/__init__.py
+++ nostalgiaforever/__init__.py
@@ -1,12 +1,12 @@
-# from pidgin.markdown import load_ipython_extension
-# load_ipython_extension()
-# from pidgin.conventions import load_ipython_extension
-# load_ipython_extension()
-#
-# with __import__('importnb').Notebook():
-#     from . import importing, testing, reuse
-#
-# from pidgin.markdown import unload_ipython_extension
-# unload_ipython_extension()
-# from pidgin.conventions import load_ipython_extension
-# unload_ipython_extension()
+from pidgin.markdown import load_ipython_extension
+load_ipython_extension()
+from pidgin.conventions import load_ipython_extension
+load_ipython_extension()
+
+with __import__('importnb').Notebook():
+    from . import importing, testing, reuse
+
+from pidgin.markdown import unload_ipython_extension
+unload_ipython_extension()
+from pidgin.conventions import load_ipython_extension
+unload_ipython_extension()
