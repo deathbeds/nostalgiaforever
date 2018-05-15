c769237234d11eda5c202f7dce442900f4e2e685
tonyfast <tony.fast@gmail.com>
2018-05-14 23:06:33 -0400

    @wd15 suggest I change my entire talk

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
+from pidgin.markdown import load_ipython_extension
+load_ipython_extension()
+from pidgin.conventions import load_ipython_extension
+load_ipython_extension()
 with __import__('importnb').Notebook():
-    from nostalgiaforever.informal import *
-    from nostalgiaforever.formal import *
-    from nostalgiaforever.continuous import *
+    from nostalgiaforever.importing import *
+    from nostalgiaforever.testing import *
+    from nostalgiaforever.reuse import *
